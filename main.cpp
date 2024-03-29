fa81f6f8329a04e69d3ce321244f8195d01fe98e
David Elmo Ross <david.elmo.ross@icloud.com>
2024-03-29 16:48:02 +0000

    Hello World First Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+This program will print HELLO WORLD on the Serial Monitor Screen
+using MBED OS5. Make sure you set the SERIAL MONITOR to 9600 Baud
+*/
+
+#include "mbed.h"               // include the MBED library
+
+// main() runs in its own thread in the OS
+int main()
+{
+    printf("Hello World\n");
+}
+
